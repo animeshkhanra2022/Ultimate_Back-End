62cc32b97fd5259ab0b0fe55f296dd580c4b2f57
Animesh Khanra <animeshkhanra2022@gmail.com>
2026-03-11 16:32:06 +0530

    Add getUserChannetProfile function to retrieve user channel details and subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -356,7 +356,75 @@ const updateUserCoverImage = asyncHandler( async(req, res) => {
     .json(new ApiResponce(201, user, "Cover image update successfully"))
 })
 
+const getUserChannetProfile = asyncHandler( async(req, res) => {
+    const {username} = req.params;
+    if(!username?.trim()) {
+        throw new ApiError(400, "Username is missing");
+    }
 
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscriberCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                username: 1,
+                fullName: 1,
+                email: 1,
+                subscriberCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1
+            }
+        }
+    ])
+
+    if(!channel?.user){
+        throw new ApiError(400, "Channel does not exists"); 
+    }
+
+
+    return res
+    .status(201)
+    .json(new ApiResponce(201, channel[0], "User channel fetched successfully"))
+    
+})
 
 
 
@@ -372,5 +440,5 @@ export { registerUser,
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-
+    getUserChannetProfile
 }
