3a14bfdc2926b4dbc36a41eca99d09f8e5b212a9
Animesh Khanra <animeshkhanra2022@gmail.com>
2026-03-12 08:05:54 +0530

    Add getWatchHistory function and update route for current user retrieval

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,7 @@ import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponce } from "../utils/ApiResponce.js"
 import jwt from "jsonwebtoken"
-// import mongoose from "mongoose";
+import mongoose from "mongoose"
 
 
 
@@ -269,7 +269,7 @@ const changeCurrentPassword = asyncHandler( async(req, res) => {
 
 })
 
-const currentUser = asyncHandler( async(req, res) => {
+const getCurrentUser = asyncHandler( async(req, res) => {
     return res
     .status(200)
     .json(new ApiResponce(200, req.user, "Current User fetch successfully!"))
@@ -427,7 +427,55 @@ const getUserChannetProfile = asyncHandler( async(req, res) => {
 })
 
 
+const getWatchHistory = asyncHandler( async(req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
 
+    return res
+    .status(200)
+    .json(
+        new ApiResponce(200, user[0].watchHistory, "Watch history fetched successfully")
+    )
+})
 
 
 
@@ -436,9 +484,10 @@ export { registerUser,
     logoutUser,
     refreshAccessToken,
     changeCurrentPassword,
-    currentUser,
+    getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannetProfile
+    getUserChannetProfile,
+    getWatchHistory
 }
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,7 +1,18 @@
 import { Router } from "express"
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
 import { upload }  from "../middlewares/multer.middlewares.js"
 import { verifyJwt } from "../middlewares/auth.middlewares.js";
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannetProfile,
+    getWatchHistory } from "../controllers/user.controller.js";
 
 
 
@@ -26,5 +37,16 @@ router.route("/login").post(loginUser);
 // SECURE ROUTES
 router.route("/logout").post(verifyJwt, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJwt, changeCurrentPassword);
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+router.route("/update-account-details").patch(verifyJwt, updateAccountDetails);
+router.route("/update-avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
+router.route("/update-coverImage").patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJwt, getUserChannetProfile);
+router.route("/watch-history").get(verifyJwt, getWatchHistory);
+
+
+
+
 
 export default router;
